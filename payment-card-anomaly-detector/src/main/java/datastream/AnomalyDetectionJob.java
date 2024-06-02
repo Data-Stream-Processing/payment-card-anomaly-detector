7b828efabe258049eadd7ba1e110ffcfe79256a8
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-06-02 17:05:22 -0400

    fix deserialization

--- payment-card-anomaly-detector/src/main/java/datastream/AnomalyDetectionJob.java
+++ payment-card-anomaly-detector/src/main/java/datastream/AnomalyDetectionJob.java
@@ -46,6 +46,6 @@ public class AnomalyDetectionJob {
 
 		transactionStream.print();
 
-		env.execute("Fraud Detection");
+		env.execute("Kafka-anomalyDetection");
 	}
 }
--- payment-card-anomaly-detector/src/main/java/utils/TransactionDeserializationSchema.java
+++ payment-card-anomaly-detector/src/main/java/utils/TransactionDeserializationSchema.java
@@ -3,7 +3,9 @@ package src.main.java.utils;
 import src.main.java.models.Transaction;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 import java.io.IOException;
 
